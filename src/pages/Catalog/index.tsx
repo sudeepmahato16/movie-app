a5ce467455d86a812cdf23246f1873b7c1468dbf
sudeepmahato16 <122378993+sudeepmahato16@users.noreply.github.com>
2024-11-17 15:02:55 +0545

    update loader

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
+import { FiLoader } from "react-icons/fi";
 
 
 import { MovieCard, SkelatonLoader } from "@/common";
@@ -68,10 +69,10 @@ const Catalog = () => {
         )}
 
         {isFetching && !isCategoryChanged ? (
-          <SkelatonLoader
-            isMoviesSliderLoader={false}
-            className="md:pt-8 sm:pt-7 pt-6"
-          />
+          <div className="my-4">
+            <FiLoader className="mx-auto dark:text-gray-300 w-5 h-5 animate-spin"/>
+          </div>
+ 
         ) : (
           <div className="w-full flex items-center justify-center">
             <button
@@ -80,7 +81,7 @@ const Catalog = () => {
                 setPage(page + 1);
               }}
               disabled={isFetching}
-              className="sm:py-2 xs:py-[6px] py-1 sm:px-4 xs:px-3 px-[10.75px] bg-[#ff0000] text-gray-50 rounded-full md:text-[15.25px] sm:text-[14.75px] xs:text-[14px] text-[12.75px] shadow-md hover:-translate-y-1 transition-all duration-300 font-medium font-nunito lg:my-8 my-7"
+              className="sm:py-2 xs:py-[6px] py-1 sm:px-4 xs:px-3 px-[10.75px] bg-[#ff0000] text-gray-50 rounded-full md:text-[15.25px] sm:text-[14.75px] xs:text-[14px] text-[12.75px] shadow-md hover:-translate-y-1 transition-all duration-300 font-medium font-nunito my-4"
             >
               Load more
             </button>
